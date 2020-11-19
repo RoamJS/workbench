e840314c5bae4afbddc72ccca7ab14ec454eca17
Glitch (roamhacker-lab) <none>
2020-11-19 19:25:51 +0000

    sb cursor empty insertion

--- main.js
+++ main.js
@@ -27,7 +27,7 @@
 if( typeof window.roam42 == 'undefined' ) { 
 
   window.roam42     =   {};
-  roam42.buildID = 'Roam<sup>42</sup> 2020-11-19 (BeebleBrox) ';
+  roam42.buildID = 'Roam<sup>42</sup> 2020-11-19b (BeebleBrox) ';
   
   roam42.host    = document.currentScript.src.replace('main.js','');
 
