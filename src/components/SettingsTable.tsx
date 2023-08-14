db6d98667b6405ddc458d4ef178e6e9e5102aa56
Michael Gartner <mclicks@gmail.com>
2023-08-14 11:00:34 -0700

    Settings UI table example (#430)
    * table example
    
    * update table
    
    * handle mobile
    
    * add SettingsTable component with multiple enhancements
    
    * minor fixes
    
    * border fix, disable Info column below 1280px
    
    * remove redundant code
    
    * review edits
    
    * 1.2.6

---
+++ src/components/SettingsTable.tsx
@@ -0,0 +1,165 @@
+import React, { useState } from "react";
+import {
+  AnchorButton,
+  Button,
+  HTMLTable,
+  Popover,
+  Switch,
+} from "@blueprintjs/core";
+import { OnloadArgs } from "roamjs-components/types";
+import { Feature } from "../index";
+
+const SettingsTable =
+  (
+    FEATURES: Feature[],
+    initialSettings: { [key: string]: boolean },
+    extensionAPI: OnloadArgs["extensionAPI"]
+  ) =>
+  () => {
+    const [featureToggleSettings, setFeatureToggleSettings] =
+      useState(initialSettings);
+
+    const settingsStyle: React.CSSProperties = {
+      maxWidth: "25px",
+      minWidth: "initial",
+      textAlign: "center",
+      overflow: "hidden",
+      textOverflow: "ellipsis",
+      whiteSpace: "nowrap",
+      color: "white",
+      verticalAlign: "middle",
+    };
+    const featureStyle: React.CSSProperties = {
+      textAlign: "left",
+      color: "white",
+      verticalAlign: "middle",
+    };
+    const thBorder: React.CSSProperties = {
+      borderTop: "none",
+      borderRight: "none",
+      borderLeft: "none",
+      borderBottom: "1px solid gray",
+    };
+    const noBorder: React.CSSProperties = {
+      border: "none",
+    };
+    const isMaxWidth = window.matchMedia("(max-width: 1279px)").matches;
+
+    return (
+      <HTMLTable
+        bordered={false}
+        style={{ ...noBorder }}
+        className="workbench-settings"
+      >
+        <thead>
+          <tr style={{ ...thBorder }}>
+            <th
+              style={{
+                ...settingsStyle,
+                ...noBorder,
+                display: isMaxWidth ? "none" : "",
+              }}
+            >
+              Info
+            </th>
+            <th style={{ ...featureStyle, ...noBorder }}>Feature</th>
+            <th style={{ ...settingsStyle, ...noBorder }}>Documentation</th>
+            {/* <th style={{ ...settingsStyle }}>Settings</th> */}
+            <th style={{ ...settingsStyle, ...noBorder }}>Enabled</th>
+          </tr>
+        </thead>
+        <tbody>
+          {FEATURES.map(({ id, name, docs, module, description, gif }, i) => (
+            <tr
+              key={id}
+              style={{
+                borderBottom:
+                  i === FEATURES.length - 1 ? "none" : "solid 1px #293742",
+                borderRight: "none",
+                borderLeft: "none",
+              }}
+            >
+              <td
+                style={{
+                  ...settingsStyle,
+                  ...noBorder,
+                  display: isMaxWidth ? "none" : "",
+                }}
+              >
+                <Popover
+                  content={
+                    <div style={{ width: "540px", height: "420px" }}>
+                      <p
+                        style={{
+                          padding: "10px",
+                          margin: 0,
+                          textAlign: "center",
+                          borderBottom: "1px solid lightgray",
+                        }}
+                      >
+                        {description}
+                      </p>
+                      <img
+                        style={{
+                          width: "100%",
+                          marginTop: "10px",
+                        }}
+                        src={`https://github.com/RoamJS/workbench/blob/main/docs/media/${gif}.gif?raw=true`}
+                      />
+                    </div>
+                  }
+                >
+                  <Button icon="info-sign" />
+                </Popover>
+              </td>
+              <td style={{ ...featureStyle, ...noBorder }}>
+                <span>{name}</span>
+              </td>
+              <td style={{ ...settingsStyle, ...noBorder }}>
+                <AnchorButton
+                  intent="primary"
+                  icon="document-open"
+                  href={`https://github.com/RoamJs/workbench/blob/main/docs/${docs}`}
+                />
+              </td>
+              {/* placeholder for when settings migrated to API */}
+              {/* https://github.com/RoamJS/workbench/issues/402 */}
+              {/* <td style={{ ...settingsStyle, ...cellsBorder }}>
+                  {settings ? (
+                    <Button
+                      intent="primary"
+                      icon="cog"
+                      onClick={() => {
+                        console.log("Button clicked!");
+                      }}
+                    />
+                  ) : null}
+                </td> */}
+              <td style={{ ...settingsStyle, ...noBorder }}>
+                <Switch
+                  checked={featureToggleSettings[id]}
+                  onChange={(e) => {
+                    const updatedSettings = {
+                      ...featureToggleSettings,
+                      [id]: (e.target as HTMLInputElement).checked,
+                    };
+                    setFeatureToggleSettings(updatedSettings);
+                    extensionAPI.settings.set(
+                      id,
+                      (e.target as HTMLInputElement).checked
+                    );
+                    module.toggleFeature(
+                      (e.target as HTMLInputElement).checked,
+                      extensionAPI
+                    );
+                  }}
+                />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </HTMLTable>
+    );
+  };
+
+export default SettingsTable;
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import runExtension from "roamjs-components/util/runExtension";
 import { render as renderToast } from "roamjs-components/components/Toast";
-
+import addStyle from "roamjs-components/dom/addStyle";
 // features
 import * as alert from "./features/alert";
 import * as article from "./features/article";
@@ -19,13 +19,25 @@ import * as tagCycle from "./features/tagCycle";
 import * as tally from "./features/tally";
 import * as tutorials from "./features/tutorials";
 import * as weeklyNotes from "./features/weekly-notes";
+import SettingsTable from "./components/SettingsTable";
 
+export type Feature = {
+  id: string;
+  name: string;
+  description: string;
+  module: any;
+  docs: string;
+  gif: string;
+  // settings: any;
+};
 const FEATURES = [
   {
     id: "alert",
     name: "Alert",
     description: "Schedule reminders that are triggered by Roam.",
     module: alert,
+    docs: "alert.md",
+    gif: "short-demo-alert",
   },
   {
     id: "workBench",
@@ -33,12 +45,18 @@ const FEATURES = [
     description:
       "Whether or not to include the core set of workBench commands in the Roam Command Palette",
     module: workBench,
+    // settings: commandPalettePlusSettings,
+    docs: "command-palette-plus.md",
+    gif: "short-demo-commandpaletteplus",
   },
   {
     id: "dailyNotesPopup",
     name: "Daily Notes Popup",
     module: dailyNotesPopup,
     description: "A popup window with the current Daily Notes Page",
+    // settings: dailyNotesPopupSettings,
+    docs: "daily-note-popup.md",
+    gif: "short-demo-dnp",
   },
   {
     id: "decorators",
@@ -46,24 +64,34 @@ const FEATURES = [
     module: decorators,
     description:
       "Decorates blocks with various configurable features for quick actions.",
+    // settings: decoratedBlocksSettings,
+    docs: "decorated-blocks.md",
+    gif: "short-demo-decorated-blocks",
   },
   {
     id: "roamNavigator",
     name: "Deep Nav",
     module: roamNavigator,
     description: "Quick navigation through Roam's UI using the keyboard",
+    docs: "deep-nav.md",
+    gif: "short-demo-deep-nav",
   },
   {
     id: "dictionary",
     name: "Dictionary",
     module: dictionary,
     description: "Look up terms in the dictionary",
+    // settings: dictionarySettings,
+    docs: "dictionary.md",
+    gif: "short-demo-dictionary",
   },
   {
     id: "formatConverter",
     name: "Format Converter",
     module: formatConverter,
     description: "Outputs the current page to various formats",
+    docs: "format-converter.md",
+    gif: "short-demo-format-converter",
   },
   {
     id: "jumpNav",
@@ -71,18 +99,24 @@ const FEATURES = [
     module: jumpnav,
     description:
       "Keyboard shortcuts for interacting with the Roam user interface",
+    docs: "hot-keys.md",
+    gif: "short-demo-hot-keys",
   },
   {
     id: "ocr",
     name: "Image OCR",
     description: "Extract the text from an image and add it as child blocks!",
     module: ocr,
+    docs: "image-ocr.md",
+    gif: "short-demo-image-ocr",
   },
   {
     id: "article",
     name: "Import Article",
     description: "Add commands to import web articles directly into Roam",
     module: article,
+    docs: "import-article.md",
+    gif: "short-demo-import-article",
   },
   {
     id: "livePreview",
@@ -90,24 +124,35 @@ const FEATURES = [
     module: livePreview,
     description:
       "See live and editable preview of pages upon hovering over tags and page links",
+    // settings: livePreviewSettings,
+    docs: "live-preview.md",
+    gif: "short-demo-live-preview",
   },
   {
     id: "mindmap",
     name: "Mind Map",
     module: mindmap,
     description: "Visualize pieces of your Roam graph as a mind map!",
+    docs: "mindmap.md",
+    gif: "short-demo-mind-map",
   },
   {
     id: "privacyMode",
     name: "Privacy Mode",
     description: "Redacts content from your Roam",
     module: privacyMode,
+    // settings: privacyModeSettings,
+    docs: "privacy-mode.md",
+    gif: "short-demo-privacy-mode",
   },
   {
     id: "tag-cycle",
     name: "Tag Cycle",
     module: tagCycle,
     description: "Define custom cycles tied to a keyboard shortcut!",
+    // settings: tagCycleSettings,
+    docs: "tag-cycle.md",
+    gif: "short-demo-tag-cycle",
   },
   {
     id: "tally",
@@ -115,6 +160,8 @@ const FEATURES = [
     module: tally,
     description:
       "Introduce a tally button component to use directly in your Roam graph!",
+    docs: "tag-cycle.md",
+    gif: "short-demo-tally",
   },
   {
     id: "tutorials",
@@ -122,55 +169,69 @@ const FEATURES = [
     module: tutorials,
     description:
       "Learn how to use WorkBench features and Roam basics right from within Roam",
+    docs: "tutorials.md",
+    gif: "short-demo-tutorials",
   },
   {
     id: "weekly-notes",
     name: "Weekly Notes",
     module: weeklyNotes,
     description: "Enabling workflows surrounding weekly note pages.",
+    // settings: weeklyNotesSettings,
+    docs: "weekly-notes.md",
+    gif: "short-demo-weekly-note",
   },
 ];
 
 export default runExtension(async ({ extensionAPI, extension }) => {
   tutorials.setVersion(extension.version);
 
-  extensionAPI.settings.panel.create({
-    tabTitle: "WorkBench",
-    settings: FEATURES.map((f) => ({
-      id: f.id,
-      description: f.description,
-      name: f.name,
-      action: {
-        type: "switch",
-        onChange: (e) => f.module.toggleFeature(e.target.checked, extensionAPI),
-      },
-    })),
-  });
-
-  const flags: boolean[] = [];
-
+  const initialSettings: { [key: string]: boolean } = {};
   FEATURES.forEach(({ id, module }) => {
     const flag = extensionAPI.settings.get(id);
-    const unset = typeof flag === "undefined" || flag === null;
-    if (unset) extensionAPI.settings.set(id, false);
-    flags.push(unset || flag === false ? false : (flag as boolean));
-    module.toggleFeature(unset ? false : (flag as boolean), extensionAPI);
+    initialSettings[id] = !!flag;
+    module.toggleFeature(flag as boolean, extensionAPI);
   });
 
-  if (!flags.some((flag) => flag)) {
+  extensionAPI.settings.panel.create({
+    tabTitle: "WorkBench",
+    settings: [
+      {
+        id: "id",
+        description: "",
+        name: "WorkBench Features",
+        action: {
+          type: "reactComponent",
+          component: SettingsTable(FEATURES, initialSettings, extensionAPI),
+        },
+      },
+    ],
+  });
+
+  // Hide panel "name"
+
+  const style = addStyle(`
+  [id*="dvargas92495"][id*="workbench"] section.rm-settings-panel__section.flex-h-box.rm-settings-panel__section--top > div > h4 { 
+    display: none; }
+  `);
+
+  if (!Object.values(initialSettings).some((flag) => flag)) {
     renderToast({
       id: "roamjs-workbench-features-disabled",
       // code format below intentional to preserve newlines
       content: `WorkBench features are **disabled** by default.
 
 Enable them in the WorkBench settings tab!
-      `,
+    `,
     });
   }
 
-  return () => {
-    FEATURES.forEach(({ module }) => {
-      module.toggleFeature(false, extensionAPI);
-    });
+  return {
+    elements: [style],
+    unload: () => {
+      FEATURES.forEach(({ module }) => {
+        module.toggleFeature(false, extensionAPI);
+      });
+    },
   };
 });
