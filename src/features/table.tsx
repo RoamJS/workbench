efe845f2d96f109b2b4b8842641166bb786642c5
Michael Gartner <mclicks@gmail.com>
2024-01-02 17:36:56 -0600

    Add Create WorkBench Table command

--- src/features/table.tsx
+++ src/features/table.tsx
@@ -33,6 +33,8 @@ import getSubTree from "roamjs-components/util/getSubTree";
 import getUids from "roamjs-components/dom/getUids";
 import setInputSetting from "roamjs-components/util/setInputSetting";
 import setInputSettings from "roamjs-components/util/setInputSettings";
+import { OnloadArgs } from "roamjs-components/types";
+import { render as renderToast } from "roamjs-components/components/Toast";
 
 type ConfigurationProps = {
   blockUid: string;
@@ -577,7 +579,6 @@ const DisplayTable = ({ blockUid, setIsEdit }: DisplayTableProps) => {
                             top: 0,
                             right: 0,
                             bottom: 0,
-                            borderRight: `1px solid rgba(16,22,26,0.15)`,
                             paddingLeft: 5,
                             pointerEvents: "auto",
                           }}
@@ -616,7 +617,10 @@ const Table = ({ blockUid }: { blockUid: string }): JSX.Element => {
 };
 
 const unloads = new Set<() => void>();
-export const toggleFeature = (flag: boolean) => {
+export const toggleFeature = (
+  flag: boolean,
+  extensionAPI: OnloadArgs["extensionAPI"]
+) => {
   if (flag) {
     const tableButtonObserver = createButtonObserver({
       attribute: "wb-table",
@@ -627,6 +631,29 @@ export const toggleFeature = (flag: boolean) => {
         )(b);
       },
     });
+
+    extensionAPI.ui.commandPalette.addCommand({
+      label: "Create WorkBench Table",
+      callback: async () => {
+        const uid = window.roamAlphaAPI.ui.getFocusedBlock()?.["block-uid"];
+        if (!uid) {
+          renderToast({
+            id: "workbench-table-create-block",
+            content: "Must be focused on a block to create a WorkBench Table",
+          });
+          return;
+        }
+        // setTimeout is needed because sometimes block is left blank
+        setTimeout(async () => {
+          await updateBlock({
+            uid,
+            text: "{{wb-table}}",
+          });
+        }, 200);
+        document.querySelector("body")?.click();
+      },
+    });
+
     addStyle(`
       /* Chrome, Safari, Edge, Opera */
       .roamjs-workbench-table-config input::-webkit-outer-spin-button,
@@ -658,6 +685,7 @@ export const toggleFeature = (flag: boolean) => {
         display: none;
       }
     `);
+
     unloads.add(() => tableButtonObserver.disconnect());
   } else {
     unloads.forEach((u) => u());
